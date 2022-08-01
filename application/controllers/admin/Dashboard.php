ad8f9c449f5eb63024c43c053a8a325c86e9073c
Navin Ahir <ahirnavin49@gmail.com>
2022-08-01 22:17:16 +0530

    remove code

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model(array('admin/users_model','admin/dashboard_model','admin/lead_model', 'admin/product_model', 'admin/inventory_model'));
+        $this->load->model(array('admin/users_model','admin/dashboard_model','admin/lead_model', 'admin/product_model'));
     }
 
     /**
@@ -21,8 +21,8 @@ class Dashboard extends MY_Controller {
             redirect(base_url(), 'refresh');
         }
         $data['title'] = 'Dashboard';
-        $data['companyArr'] = $this->dashboard_model->get_all_details(TBL_COMPANY, array('is_delete' => 0, 'status' => 'active'),array(array('field' => 'name', 'type' => 'asc')))->result_array();
-        $data['yearArr'] = $this->dashboard_model->get_all_details(TBL_YEAR, array('is_delete' => 0))->result_array();
+//        $data['companyArr'] = $this->dashboard_model->get_all_details(TBL_COMPANY, array('is_delete' => 0, 'status' => 'active'),array(array('field' => 'name', 'type' => 'asc')))->result_array();
+//        $data['yearArr'] = $this->dashboard_model->get_all_details(TBL_YEAR, array('is_delete' => 0))->result_array();
         $data['totalLead'] = $this->lead_model->get_lead('count');
         $data['totalLead'] = $this->lead_model->get_lead('count');
         $data['totalUsers'] = $this->users_model->get_ajax_data('count');
